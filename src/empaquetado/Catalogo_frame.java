c0d1b0e1b4c073b3627e6e2d6fd8e295cad7c552
daniel071286 <danieldvz2007@gmail.com>
2026-05-01 23:51:52 -0500

    Botón para ver estado del carrito

--- src/empaquetado/Catalogo_frame.java
+++ src/empaquetado/Catalogo_frame.java
@@ -4,11 +4,16 @@
  */
 package empaquetado;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author ASUS
  */
 public class Catalogo_frame extends javax.swing.JFrame {
+    
+    String producto1="Camiseta", producto2="Zapatos";
+            
 
     /**
      * Creates new form Catalogo_frame
@@ -34,6 +39,7 @@ public class Catalogo_frame extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -61,6 +67,13 @@ public class Catalogo_frame extends javax.swing.JFrame {
         jLabel8.setFont(new java.awt.Font("Franklin Gothic Book", 0, 14)); // NOI18N
         jLabel8.setText("Pantaloneta");
 
+        jButton1.setText("Ver carrito");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -72,13 +85,14 @@ public class Catalogo_frame extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel4)
                             .addComponent(jLabel5)
-                            .addComponent(jLabel6)
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jLabel2)
-                                    .addComponent(jLabel3))
+                                    .addComponent(jLabel3)
+                                    .addComponent(jLabel6))
                                 .addGap(117, 117, 117)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jButton1)
                                     .addComponent(jLabel8)
                                     .addComponent(jLabel7)))))
                     .addGroup(layout.createSequentialGroup()
@@ -103,13 +117,20 @@ public class Catalogo_frame extends javax.swing.JFrame {
                 .addComponent(jLabel4)
                 .addGap(52, 52, 52)
                 .addComponent(jLabel5)
-                .addGap(27, 27, 27)
-                .addComponent(jLabel6)
-                .addContainerGap(75, Short.MAX_VALUE))
+                .addGap(24, 24, 24)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(jButton1))
+                .addContainerGap(71, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        
+        JOptionPane.showMessageDialog(null,"Productos en carrito: \n" + producto1+"\n"+producto2);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -147,6 +168,7 @@ public class Catalogo_frame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
